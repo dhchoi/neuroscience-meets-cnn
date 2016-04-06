f5d0225f108956f732b3da199040384d79a52d51
Yongjin <yjc04@hotmail.com>
2016-04-06 02:28:47 -0400

    adding loops to test each subject iteratively

--- lr_sci.py
+++ lr_sci.py
@@ -6,7 +6,9 @@ import pickle
 from sklearn import linear_model
 
 # import some data to play with
-fx = open('x_sparse.p')
+#fx = open('x_sparse.p')
+#fx = open('x_pca.p')
+fx = open('x_lda.p')
 fy = open('y.p')
 
 X = pickle.load(fx)
@@ -15,9 +17,20 @@ Y = pickle.load(fy)
 temp = np.array([range(0,60)])
 Y = (Y.dot(temp.T)).ravel()
 
-h = .02  # step size
-
 #logreg = linear_model.LogisticRegression(penalty='l2', solver='sag', verbose=1)
-logreg = linear_model.LogisticRegression(penalty='l2', solver='newton-cg', n_jobs = 4, verbose=1)
+logreg = linear_model.LogisticRegression(penalty='l2', solver='newton-cg', n_jobs = -1, verbose=1)
 
-logreg.fit(X[range(360),:], Y[range(360)])
+num_subjects = 9
+tot = 0
+for i in range(num_subjects):
+    print "=== using subject", i+1, "as test set"
+    X = np.concatenate((X[360:], X[:360]), axis=0)
+    Y = np.concatenate((Y[360:], Y[:360]), axis=0)
+    #print X[:X.shape[0]-360,:].shape
+    #print Y[:Y.shape[0]-360].shape
+    logreg.fit(X[:X.shape[0]-360,:], Y[:Y.shape[0]-360])
+    acc = sum(logreg.predict(X[3240-360:,:])==Y[3240-360:]) / 360.0
+    print "accuracy : ", acc
+    tot += acc
+
+print "mean accuracy : ", tot / (1.0 * num_subjects)
