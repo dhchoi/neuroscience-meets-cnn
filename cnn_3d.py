1e1b3b6f529123c3be5c5f72f0d5f1a3c8615969
yongjin <ycho1@andrew.cmu.edu>
2016-07-30 23:11:25 -0400

    added lines for logging via tensorboard

--- cnn_3d.py
+++ cnn_3d.py
@@ -8,7 +8,7 @@ import preprocess as prep
 import time
 
 num_labels = 60
-num_steps = 1000000
+num_steps = 100000
 
 batch_size = 15
 patch_size = 5
@@ -45,18 +45,20 @@ class ConvolutionalNetwork:
 
     def set_datasets(self, data, labels):
         num_total_data = len(data)
-        num_train_offset = num_total_data / 9 * 7
-        #num_train_offset = num_total_data / 6 * 5
-        num_valid_offset = num_train_offset + (num_total_data / 9)
+        #num_train_offset = num_total_data / 9 * 7
+        num_train_offset = num_total_data / 6 * 5
+        num_valid_offset = num_train_offset + (num_total_data / 6 )
         #num_valid_offset = num_train_offset
-        num_test_offset = num_valid_offset + (num_total_data / 9)
+        num_test_offset = num_valid_offset
 
         self.train_dataset = (self.reformat(data[0:num_train_offset, :, :, :]))
         self.train_labels = labels[0:num_train_offset, :]
-        self.valid_dataset = (self.reformat(data[num_train_offset:num_valid_offset, :, :, :]))
-        self.valid_labels = labels[num_train_offset:num_valid_offset, :]
-        self.test_dataset = (self.reformat(data[num_valid_offset:, :, :, :]))
-        self.test_labels = labels[num_valid_offset:, :]
+        #self.valid_dataset = (self.reformat(data[num_train_offset:num_valid_offset, :, :, :]))
+        self.valid_dataset = (self.reformat(data[num_train_offset:num_valid_offset:50, :, :, :]))
+        self.valid_labels = labels[num_train_offset:num_valid_offset:50, :]
+        #self.test_dataset = (self.reformat(data[num_valid_offset:, :, :, :]))
+        self.test_dataset = (self.reformat(data[num_valid_offset:-1:50, :, :, :]))
+        self.test_labels = labels[num_valid_offset:-1:50, :]
 
         print 'Training set', self.train_dataset.shape, self.train_labels.shape
         print 'Validation set', self.valid_dataset.shape, self.valid_labels.shape
@@ -78,6 +80,11 @@ class ConvolutionalNetwork:
 
         def max_pool3d(input, k, s):
             return tf.nn.max_pool3d(input, ksize=[1,k,k,k,1], strides=[1,s,s,s,1], padding='SAME')
+
+        def batch_norm(tensor):
+            #m, v = tf.nn.moments(tensor, [0])
+            m, v = tf.nn.moments(tensor, [0,1,2,3])
+            return tf.nn.batch_normalization(tensor, m, v, None, None, 1e-12)
         
         graph = tf.Graph()
 
@@ -111,17 +118,19 @@ class ConvolutionalNetwork:
             weights4 = weight_var([num_hidden, num_labels])
             biases4 = bias_var([num_labels])
 
-
-                    
-            
             # Model.
-            def model(data, dropout=True):
+            def model(data, dropout=False):
                 # do dropout on the input data itself i.e. blankout 
-                data = tf.nn.dropout(data, 0.5)
+                #data = tf.nn.dropout(data, 0.5)
+
+                # batch_normalization
+                #data = batch_norm(data)
                 
                 # Layer 1 (conv): data = `patch_size` x `patch_size` x `image_size_z`
                 print "conv1.shape (before conv)", data.get_shape()
+                #pdb.set_trace()
                 conv1 = conv3d(data, weights1)  # conv = `patch_size` x `patch_size` x `out_features1`
+                conv1 = batch_norm(conv1)
                 conv1 = tf.nn.relu(conv1 + biases1)
                 print "conv1.shape (after conv)", conv1.get_shape()
                 #conv1 = max_pool_kxk(conv1, 2)  # pool = `patch_size`/2 x `patch_size`/2 x `out_features1`
@@ -133,6 +142,7 @@ class ConvolutionalNetwork:
                 # Layer 2 (conv): data = `patch_size`/2 x `patch_size`/2 x `out_features1`
                 print "conv2.shape (before conv)", conv1.get_shape()
                 conv2 = conv3d(conv1, weights2)  # conv = `patch_size` x `patch_size` x `out_features2`
+                conv2 = batch_norm(conv2)
                 conv2 = tf.nn.relu(conv2 + biases2)
                 print "conv2.shape (after conv)", conv2.get_shape()
                 conv2 = max_pool3d(conv2, 3, 3)  # pool = `patch_size`/2/2 x `patch_size`/2/2 x `out_features2`
@@ -146,6 +156,7 @@ class ConvolutionalNetwork:
                 print "dense1.shape (input)", reshape.get_shape()
                 print "dense1.shape (weights)", weights3.get_shape()
                 dense1 = tf.nn.relu(tf.matmul(reshape, weights3) + biases3)
+                #dense1 = batch_norm(dense1)
                 if dropout:
                     dense1 = tf.nn.dropout(dense1, dropout)
 
@@ -157,16 +168,22 @@ class ConvolutionalNetwork:
             self.loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits, tf_train_labels))
 
             # Optimizer.
-            # self.optimizer = tf.train.GradientDescentOptimizer(0.0001).minimize(self.loss)
-            self.optimizer = tf.train.AdamOptimizer(0.001).minimize(self.loss)
+            #self.optimizer = tf.train.GradientDescentOptimizer(0.00001).minimize(self.loss)
+            self.optimizer = tf.train.AdamOptimizer(0.00005).minimize(self.loss)
             
             # Predictions for the training, validation, and test data.
             self.train_prediction = tf.nn.softmax(logits)
+            correct_prediction = tf.equal(self.train_prediction, tf_train_labels)
+            train_acc = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+            tf.scalar_summary('minibatch accuracy', train_acc)
+            merged = tf.merge_all_summaries()
+            
             self.valid_prediction = tf.nn.softmax(model(tf_valid_dataset, False))
             self.test_prediction = tf.nn.softmax(model(tf_test_dataset, False))
 
         with tf.Session(graph=graph) as session:
-            f_log = open('blankout.log' , 'wb')
+            #f_log = open('cropping.log' , 'wb')
+            train_writer = tf.train.SummaryWriter('/home/ycho1/Desktop/', session.graph)
             tf.initialize_all_variables().run()
             print 'initialization done'
             for step in range(num_steps):
@@ -175,17 +192,18 @@ class ConvolutionalNetwork:
                 #batch_data = preprocess(batch_data)
                 batch_labels = self.train_labels[offset:(offset + batch_size), :]
                 feed_dict = {tf_train_dataset: batch_data, tf_train_labels: batch_labels}
-                _ , l, predictions = session.run([self.optimizer, self.loss, self.train_prediction], feed_dict=feed_dict)
-                #summary, predictions = session.run([self.optimizer, self.loss, self.train_prediction], feed_dict=feed_dict)
+                #_ , l, predictions = session.run([self.optimizer, self.loss, self.train_prediction], feed_dict=feed_dict)
+                summary, l, predictions = session.run([merged, self.loss, self.train_prediction], feed_dict=feed_dict)
                 if (step % 50 == 0):
+                    train_writer.add_summary(summary, step)
                     print('Minibatch loss at step %d: %f' % (step, l))
                     minibatch_acc = accuracy(predictions, batch_labels)
                     print('Minibatch accuracy: %.1f%%' % minibatch_acc)
                     val_acc = accuracy(self.valid_prediction.eval(), self.valid_labels)
                     print('Validation accuracy: %.1f%%' % val_acc)
-                    test_acc = accuracy(self.test_prediction.eval(), self.test_labels)
-                    print('Test accuracy: %.1f%%' % test_acc)
-                    f_log.write(('%d, %f, %.1f%% , %.1f%%, %.1f%% \n' % (step, l, minibatch_acc, val_acc, test_acc)))
+                    #test_acc = accuracy(self.test_prediction.eval(), self.test_labels)
+                    #print('Test accuracy: %.1f%%' % test_acc)
+                    #f_log.write(('%d, %f, %.1f%% , %.1f%%, %.1f%% \n' % (step, l, minibatch_acc, val_acc, test_acc)))
 
 
 if __name__ == "__main__":
